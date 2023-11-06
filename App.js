371f0c6c9cda7191f825107664f5a176dcb18460
ngminhhieu211003 <minhhieu21033@gmail.com>
2023-11-07 00:22:43 +0700

    a

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import SignedInStack from "./navigation";
-import ProfileScreen from "./screens/ProfileScreen";
+
 
 export default function App() {
     return <SignedInStack />;
